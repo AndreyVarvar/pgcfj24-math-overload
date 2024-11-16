d0d8b37aaaed0449910801bd8e1da54ff6acf67e
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2024-11-16 23:26:54 +0200

    added 'Font' class to manage fonts

---
+++ src/UI/text.py
@@ -0,0 +1,11 @@
+from src.ui_element import UIElement
+from src.input_data import InputData
+from src.scene import Scene
+from src.font import Font
+import pygame as pg
+
+
+
+class Text():
+    def __init__(self, text, font):
+        pass
--- src/graph.py
+++ src/graph.py
@@ -12,7 +12,7 @@ class Graph():
         self.points = set()
         self.formula = "y = cos(x)"
 
-        self.update_graph = True
+        self.update_graph = False
         self.graphing_progress = 128
 
         self.error_message = ""
@@ -27,21 +27,20 @@ class Graph():
         # self.formula = graph_scene.elements["text input"]
         if self.update_graph:
             self.update_graph = False
-            self.points.clear()
             self.graphing_progress = 0
-            self.formula = "y = cos(x)"
+            self.formula = ["y = abs(sin(x))", "y=E**x"][len(self.points)>0]
             self.formula: str = self._process_formula('x', to_swap='x')  # here i use the function to just bring everything to the left hand side
             self.formula = sympy.parsing.sympy_parser.parse_expr(self.formula)
-
-            self.formula_x_diff = sympy.diff(self.formula, 'x')
 
         try:
             if self.graphing_progress < self.total_drawing_progress:
                 if self.graphing_progress < self.x_drawing_progress: # graphing_progress keeps track of what point (x or y) we are plotting right now
                     x = self.graphing_progress - 32  # here, graphing progress keeps track of what x we are checking right now
 
+                    self.remove_points_with_specific_x(x+32)
+
                     formula = self.formula.subs('x', x)  # replaces x with whatever value we are checking right now
-                    solutions = self._solve_expr(formula, 'y', self.formula_x_diff, 'x', x)
+                    solutions = self._solve_expr(formula, 'y')
 
                     for solution in solutions:
                         self.points.add((32+x, 32-round(solution)))
@@ -51,7 +50,7 @@ class Graph():
                     y = self.graphing_progress - 64 - 32 # and here as well, graphing progress keeps track of what y we are checking right now
 
                     formula = self.formula.subs('y', y)  # replaces y with whatever value we are checking right now
-                    solutions = self._solve_expr(formula, 'x', self.formula_x_diff, 'y', y)
+                    solutions = self._solve_expr(formula, 'x')
 
                     for solution in solutions:
                         self.points.add((32+round(solution), 32-y))
@@ -63,24 +62,20 @@ class Graph():
             print(e)
         except TypeError as e:
             self.graphing_progress += 1
-            self.error_message = "We dont support variables"
-            print("NO VARIABLES: ", e)
+            self.error_message = "Something went wrong"
+            print("Something went wrong: ", e)
 
         
         if parent_scene.elements["start graphing button"].was_clicked:
             self.update_graph = True
 
     
-    def _solve_expr(self, expr, unknown, diff, diff_unknown, curr):
+    def _solve_expr(self, expr, unknown):
         """solves an expression (finds all solutions it can)
 
         Args:
             expr (sympy expression): sympy expression
             unknown (str): what value do we want to find (wither 'x' of 'y')
-            diff (sympy expression): differential of the expression (we calculate it ahead of time outside the function to not waste unnecessary resourses)
-            diff_unknown (str): what value do we substitute in the differential
-            curr (int): for what value (either 'x' or 'y') are we currently finding solutions? For example, if 'unknow' is 'x', then 'curr' is 'y' 
-
         Returns:
             list: solutons
         """
@@ -88,6 +83,7 @@ class Graph():
         solutions = []
 
         val = -32
+        prev_val = -33
 
         while val < 32:
             expr1 = expr.subs(unknown, val)
@@ -100,10 +96,11 @@ class Graph():
             if abs(result) < 0.9:
                 solutions.append(val)
 
-            rate_of_change = diff.subs(unknown, val).subs(diff_unknown, curr).evalf()
-            step = 1 if ((rate_of_change == 0) or (abs(result) > 32)) else (3/abs(rate_of_change))
+            rate_of_change = (expr1 - expr.subs(unknown, prev_val)).evalf()/(val-prev_val)  # rough differential definition. Math is useful! (i mean, this is a game where math is needed, so who am i kidding)
+            step = 1 if ((rate_of_change == 0) or (abs(result) > 32)) else (1/abs(rate_of_change))
             
-            val += max(min(step, 1), 0.2)
+            prev_val = val
+            val += max(min(step, 1), 0.1)
         return solutions
 
     def _process_formula(self, val, to_swap: str) -> str:  # process the formula expression for SymPy to evaluate
@@ -139,3 +136,11 @@ class Graph():
             elif self.graphing_progress < self.y_drawing_progress:
                 line_progress = 128 - (self.graphing_progress) / ((self.total_drawing_progress) // (2*self.graph_width))
                 pg.draw.line(destination, PALLETTE["gray"], (0, line_progress), (64, line_progress))
+    
+    def remove_points_with_specific_x(self, x):
+        points_to_remove = set()
+        for p in self.points:
+            if p[0] == x:
+                points_to_remove.add(p)
+        
+        self.points = self.points - points_to_remove
